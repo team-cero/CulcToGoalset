076a68521277e3522197f599417d2b715496d563
naodps <nao.yamato@dpschool.io>
2020-02-19 13:56:55 +0100

    depend spinners

--- app/src/main/java/com/example/culctogoalset/MainActivity.kt
+++ app/src/main/java/com/example/culctogoalset/MainActivity.kt
@@ -9,9 +9,12 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
 import android.widget.ArrayAdapter
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     // variables
     companion object{
@@ -23,93 +26,117 @@ class MainActivity : AppCompatActivity() {
 
     // initializations
     //spinners
-    private val blandItems = arrayOf("Alfa Romeo", "BMW", "TOYOTA")
-    var selectedBrand: String = "Alfa Romeo"
+    var spinner1: Spinner? = null
+    var spinner2: Spinner? = null
 
-    private val modelItems = arrayOf("147 1.9", "Brera 2.2", "GT 2.0")
-    var selectedModel: String = "147 1.9"
-
-    private val fuelItems = arrayOf("Biodiesel", "Diesel","Petrol")
+    private val fuelItems = arrayOf("Biodiesel", "Diesel", "Petrol")
     var selectedFuel: String = "Biodiesel"
 
     //form
     private val DISTANCE = 1000
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //spinners
-        val spinner1 = findViewById<Spinner>(R.id.spinner1)
-        val spinner2 = findViewById<Spinner>(R.id.spinner2)
         val spinner3 = findViewById<Spinner>(R.id.spinner3)
 
         // ArrayAdapter
-        val adapter1 = ArrayAdapter(applicationContext,
-            android.R.layout.simple_spinner_item, blandItems)
-        val adapter2 = ArrayAdapter(applicationContext,
-            android.R.layout.simple_spinner_item, modelItems)
-        val adapter3 = ArrayAdapter(applicationContext,
-            android.R.layout.simple_spinner_item, fuelItems)
+        val adapter3 = ArrayAdapter(
+            applicationContext,
+            android.R.layout.simple_spinner_item, fuelItems
+        )
 
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         //set adapters to spinners
-        spinner1.adapter = adapter1
-        spinner2.adapter = adapter2
         spinner3.adapter = adapter3
 
         //register listeners
-        //Bland
-        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            //　when the item was selected
-            override fun onItemSelected(parent: AdapterView<*>?,
-                                        view: View?, position: Int, id: Long) {
-                val spinnerParent = parent as Spinner
-                val item = spinnerParent.selectedItem as String
-                // Kotlin Android Extensions
-                selectedBrand = item
-            }
-            //if the item was not selected
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                //
-            }
-        }
-
+        //Brand
         //Model
-        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            //　when the item was selected
-            override fun onItemSelected(parent: AdapterView<*>?,
-                                        view: View?, position: Int, id: Long) {
-                val spinnerParent = parent as Spinner
-                val item = spinnerParent.selectedItem as String
-                // Kotlin Android Extensions
-                selectedModel = item
-            }
-            //if the item was not selected
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                //
-            }
-        }
+
+        spinner1 = findViewById(R.id.spinner1) as Spinner
+        spinner2 = findViewById(R.id.spinner2) as Spinner
+        val adapter1 = ArrayAdapter.createFromResource(
+            this,
+            R.array.array1, android.R.layout.simple_spinner_item
+        )
+        spinner1?.setAdapter(adapter1)
+        spinner1?.setOnItemSelectedListener(this)
 
         //Fuel
-        spinner3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spinner3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             //　when the item was selected
-            override fun onItemSelected(parent: AdapterView<*>?,
-                                        view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?, position: Int, id: Long
+            ) {
                 val spinnerParent = parent as Spinner
                 val item = spinnerParent.selectedItem as String
                 // Kotlin Android Extensions
-                selectedFuel= item
+                selectedFuel = item
             }
+
             //if the item was not selected
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 //
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            // Inflate the main; this adds items to the action bar if it is present.
+            menuInflater.inflate(R.menu.main, menu)
+            return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            val id = item.getItemId()
+            return if (id == R.id.action_settings) {
+                true
+            } else super.onOptionsItemSelected(item)
+    }
+
+    override fun onItemSelected(
+            parent: AdapterView<*>, view: View, position: Int,
+            id: Long
+    ) {
+            if(spinner1?.getSelectedItem() == "Alfa Romeo") {
+                Toast.makeText(
+                    applicationContext, "Mobil dipilih",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                val adapter2 = ArrayAdapter.createFromResource(
+                    this,
+                    R.array.AlfaRomeo_array, android.R.layout.simple_spinner_item
+                )
+                spinner2?.setAdapter(adapter2)
+            } else if (spinner1?.getSelectedItem() == "BMW") {
+                val adapter2 = ArrayAdapter.createFromResource(
+                    this,
+                    R.array.BMW_array, android.R.layout.simple_spinner_item
+                )
+                spinner2?.setAdapter(adapter2)
+            } else {
+                val adapter2 = ArrayAdapter.createFromResource(
+                    this,
+                    R.array.TOYOTA_array, android.R.layout.simple_spinner_item
+                )
+                spinner2?.setAdapter(adapter2)
+            }
+
+
+        }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
 
         /*
         //move to the sub page once the culc button was tapped
@@ -134,4 +161,4 @@ class MainActivity : AppCompatActivity() {
          */
 
     }
-}
+
